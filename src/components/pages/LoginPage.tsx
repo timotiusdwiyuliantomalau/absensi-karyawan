3febc1818663ec9d7759b0665c851538c16bbe9d
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-02-06 13:25:24 +0700

    Update

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -5,14 +5,13 @@ import { RootState } from "../../../slice/store";
 import { LoadingElement } from "../ui/LoadingElement";
 import { useDispatch } from "react-redux";
 import { setIsLoading } from "../../../slice/appSlice";
-import { setCookie } from "../../utils/cookies"; 
+import { setCookie } from "../../utils/cookies";
 import Swal from "sweetalert2";
 import { getPersonalKaryawan } from "../../firebase/service";
 
 export default function LoginPage() {
   const isLoading = useSelector((state: RootState) => state.slice.isLoading);
   const dispatch = useDispatch();
-  
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-[#ed6437] to-[#dab455] text-white">
@@ -57,19 +56,23 @@ export default function LoginPage() {
         className="flex gap-3 items-center px-6 py-2 rounded-xl bg-white/10 backdrop-blur-lg shadow-lg border border-white/20 text-white font-semibold text-lg transition-transform transform hover:scale-105 active:scale-95 mt-2"
         onClick={() => {
           handleGoogleSignIn().then((data: any) => {
-            getPersonalKaryawan(data.email).then((data: any) => {  
-                  if(data.email){
-                    dispatch(setIsLoading());
-                    setCookie("myData", JSON.stringify(data))
-                    // window.location.href = "/#/home";
-                  }else{
-                    Swal.fire({
-                      icon: "error",
-                      title: "Email Tidak Terdaftar",
-                      text: "Gunakan email yang sudah terdaftar sebelumnya!",
-                    });
-                  }
+            dispatch(setIsLoading());
+            setTimeout(() => {
+              dispatch(setIsLoading());
+            }, 2000);
+            getPersonalKaryawan(data.email).then((data: any) => {
+              if (data.email) {
+                setCookie("myData", JSON.stringify(data));
+
+                window.location.href = "/#/home";
+              } else {
+                Swal.fire({
+                  icon: "error",
+                  title: "Email Tidak Terdaftar",
+                  text: "Gunakan email yang sudah terdaftar sebelumnya!",
                 });
+              }
+            });
           });
         }}
       >
