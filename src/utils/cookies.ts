eddb05fa1935f28c9be3f44fd485716e63e11074
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-02-07 10:24:46 +0700

    update

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -4,6 +4,7 @@ import Cookies from "js-cookie";
 // Set a cookie
 export const setCookie = (key:string, value:string) => {
   Cookies.set(key, value, { expires:1 });
+  return "oke"
 };
 
 // Get a cookie
