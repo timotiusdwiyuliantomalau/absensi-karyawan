4f506464ba7d735cf7464183b4444700474b53d4
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-02-13 08:31:44 +0700

    Update

--- src/components/pages/RekapKunjunganKaryawan.tsx
+++ src/components/pages/RekapKunjunganKaryawan.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { getDataKunjungan } from "../../firebase/service";
 import LoadingRefresh from "../ui/LoadingRefresh";
+import { CalendarDays } from "lucide-react";
 
 export default function RekapKunjunganKaryawan() {
-  const [dataKunjungan, setDataKunjungan] = useState<any>([]);
+  const [dataKunjungan, setDataKunjungan] = useState<any>(undefined);
   const date = new Date();
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
@@ -20,11 +21,12 @@ export default function RekapKunjunganKaryawan() {
 
   return (
     <div className="flex flex-col items-center w-3/4 desktop:w-1/2 mx-auto">
-      <h1 className="text-3xl font-bold mb-6 text-center mt-5">
+      <h1 className="text-2xl font-bold text-center mt-7">
         Rekap Kunjungan Karyawan
       </h1>
+      <p className="text-lg font-bold text-blue-600 flex gap-1 mb-2 mt-1"><CalendarDays></CalendarDays> {formattedDate}</p>
       <div className="flex flex-col bg-white gap-5">
-        {dataKunjungan.length > 0 ? (
+        {dataKunjungan ? (
           dataKunjungan.map((data: any, i: number) => (
             <div key={i} className="bg-orange-400 p-4 rounded-xl">
               <span className="flex justify-between">
@@ -32,14 +34,24 @@ export default function RekapKunjunganKaryawan() {
                   {data.nama.toUpperCase()}
                 </p>
               </span>
-              <p className="">{data.divisi.toUpperCase()}</p>
-              <p className="text-sm tablet:text-2xl font-semibold text-center">{data.deskripsi_kunjungan}</p>
-              <p className="bg-blue-600 text-white font-bold flex place-self-center w-fit p-1 rounded-lg mb-3">{data.waktu}</p>
+              <p>{data.divisi.toUpperCase()}</p>
+              <p className="">{data.alamat}</p>
+              <p className="text-sm tablet:text-2xl font-semibold text-center">
+                {data.deskripsi_kunjungan}
+              </p>
+              <p className="bg-blue-600 text-white font-bold flex place-self-center w-fit p-1 rounded-lg mb-3">
+                {data.waktu}
+              </p>
               <img src={data.img} alt="" />
             </div>
           ))
         ) : (
           <LoadingRefresh />
+        )}
+        {dataKunjungan&&dataKunjungan.length < 1 && (
+          <p className="text-red-500 text-xl font-semibold">
+            tidak ada kunjungan hari ini..
+          </p>
         )}
       </div>
     </div>
